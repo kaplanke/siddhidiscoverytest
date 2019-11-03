804c86ecc906c3d6f24545ab9baa9dad5a0381a4
kkaplan <kkaplan@borusan.com>
2019-11-03 18:17:55 +0300

    organize imports

--- src/main/java/com/siddhidiscovery/SiddhiEurekaTest.java
+++ src/main/java/com/siddhidiscovery/SiddhiEurekaTest.java
@@ -1,19 +1,8 @@
 package com.siddhidiscovery;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import io.siddhi.core.SiddhiAppRuntime;
-import io.siddhi.core.SiddhiManager;
-import io.siddhi.core.event.Event;
-import io.siddhi.core.stream.input.InputHandler;
-import io.siddhi.core.stream.output.StreamCallback;
-import io.siddhi.core.util.EventPrinter;
-import io.siddhi.core.util.transport.InMemoryBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-import javax.annotation.PostConstruct;
-import java.util.logging.Logger;
 
 
 @EnableDiscoveryClient
